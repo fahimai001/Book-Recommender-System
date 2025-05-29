bb5cdd7dc80f57cd0129915bd14ec94a532084a8
Fahim Sabir <fahimsabircstec001@gmail.com>
2025-05-29 00:29:43 -0700

    improve UI

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ def index():
     return render_template('index.html', recommendations=recommendations)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
